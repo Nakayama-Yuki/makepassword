e01e02e1f14fab469135c2cb2702df54dc7fa55f
yuki <yuki.for.neutral@gmail.com>
2024-08-16 15:23:18 +0900

    change message en to ja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PasswordGenerator from "@/components/PasswordGenerator";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Password Generator</h1>
+      <h1 className="text-2xl font-bold mb-4">パスワード生成ツール</h1>
       <PasswordGenerator />
     </main>
   );
